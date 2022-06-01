747231bff2a89ea48c03f43a5aea40939773efaa
orange-alien <6963921+orange-alien@users.noreply.github.com>
2022-06-01 22:44:18 +0900

    first commit

---
+++ app/Listeners/LogInsertion.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ModelChanged;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Arr;
+
+class LogInsertion
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $model = $event->model;
+
+        // 新規作成
+        if($model->wasRecentlyCreated) { // wasRecentlyCreated が trueかつ、originalsが空のとき
+            \Log::debug('===== 新規作成 =====');
+            // 変更前の値
+            \Log::debug( null );
+            // 変更後の値
+            \Log::debug( $model->getAttributes() );
+        }
+        // 削除
+        else if(!$model->exists) {
+            \Log::debug('===== 削除 =====');
+            
+            // 変更前の値
+            \Log::debug( null );
+            // 変更後の値
+            $deletedAtColumn = $model->getDeletedAtColumn();
+            \Log::debug( $model->getAttribute($deletedAtColumn) );
+        }
+        // 更新
+        else {
+            \Log::debug('===== 更新 =====');
+            $changes = $model->getChanges();
+            $changeKyes = array_keys($changes);
+
+            // 変更前の値
+            \Log::debug( Arr::only($model->getOriginal(), $changeKyes) );
+            // 変更後の値
+            \Log::debug( $changes );
+        }
+    }
+}
